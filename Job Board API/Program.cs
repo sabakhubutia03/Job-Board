20562a44960d0c96c2cf92512662e3ec3d40b6d6
saba khubutia <bvb.xubutia@gmail.com>
2026-02-28 14:21:46 +0400

    Initial project setup: configured Swagger, Serilog, and Entity Framework Core"

---
+++ Job Board API/Program.cs
@@ -0,0 +1,32 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
