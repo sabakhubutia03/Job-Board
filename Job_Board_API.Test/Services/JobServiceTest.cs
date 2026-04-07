d8da32e5517b9797f6437313cafc4b826f71a912
sabakhubutia03 <saba.khubutia03@geolab.edu.ge>
2026-04-07 12:14:10 +0400

    test: complete unit tests for JobService CRUD operations
    Added success and error path tests for UpdateAsync
    
    Added success and error path tests for DeleteAsync
    
    Added test for GetByIdAsync (including 404 Not Found)
    
    Integrated InMemoryDatabase validation for required fields
    
    Verified AutoMapper execution using Moq

--- Job_Board_API.Test/Services/JobServiceTest.cs
+++ Job_Board_API.Test/Services/JobServiceTest.cs
@@ -84,5 +84,88 @@ public class JobServiceTest
         var jobInDb = await _db.Jobs.FirstOrDefaultAsync();
         Assert.NotNull(jobInDb);
     }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldThrowException_WhenIdIsNotFound()
+    {  
+        var invalidId = 999;
+       await Assert.ThrowsAnyAsync<ApiException>(async () => await _jobService.GetByIdAsync(invalidId));
+    }
+
+    [Fact]
+    public async Task UpdateAsync_ShouldThrowException_WhenIdIsNotFound()
+    {
+        var nonExistingId = 999;
+        var updateDto = new JobUpdateDto
+        {
+            Title = "Test-xUnit",
+            Description = "Description",
+            CompanyId = 1
+        };
+        
+        var action = () => _jobService.UpdateAsync(nonExistingId, updateDto);
+        
+        var exception = await Assert.ThrowsAnyAsync<ApiException>(action);
+        
+        await Assert.ThrowsAnyAsync<ApiException>(action);
+        Assert.Equal(404,exception.StatusCode );
+    }
+
+    [Fact]
+    public async Task UpdateAsync_ValidRequest_UpdatesJobInDatabase()
+    {
+
+        var company = new Company 
+        { 
+            Id = 1, 
+            Name = "Test Company", 
+            Address = "Tbilisi",     
+            Industry = "IT"         
+        };
+    
+        _db.Companies.Add(company);
+    
+    
+        var job = new Job { Id = 1, Title = "Old Title", CompanyId = 1 ,Description = "Description" };
+        _db.Jobs.Add(job);
+        await _db.SaveChangesAsync();
+        _db.ChangeTracker.Clear();
+
+        var updateDto = new JobUpdateDto { Title = "New Title", CompanyId = 1 };
+
+        _mapperMock.Setup(m => m.Map(updateDto, It.IsAny<Job>()));
+        _mapperMock.Setup(m => m.Map<JobDto>(It.IsAny<Job>())).Returns(new JobDto { Title = "New Title" });
+
+
+        var result = await _jobService.UpdateAsync(1, updateDto);
+
+        Assert.NotNull(result);
+        Assert.Equal("New Title", result.Title);
+    }
+
+    [Fact]
+    public async Task DeleteAsync_ShouldThrowException_WhenIdIsNotFound()
+    {
+        var nonExistingId = 999;
+        
+        var action = () => _jobService.DeleteAsync(nonExistingId);
+        var exception = await Assert.ThrowsAnyAsync<ApiException>(action);
+        
+        Assert.Equal(404, exception.StatusCode);
+        
+    }
+
+    [Fact]
+    public async Task DeleteAsync_ValidRequest_DeletesJobInDatabase()
+    {
+        var job = new Job { Id = 1, Title = "Old Title", CompanyId = 1, Description = "Description"};
+        _db.Jobs.Add(job);
+        await _db.SaveChangesAsync();
+        
+       await _jobService.DeleteAsync(job.Id);
+       
+       var deleteJob = await _db.Jobs.FindAsync(job.Id);
+       Assert.Null(deleteJob);
+    }
     
 }
